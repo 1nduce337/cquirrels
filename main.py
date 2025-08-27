0ec95827d2d903e4200e12bdfd78370e4e2dd33f
Brook A. <brook2022nets@gmail.com>
2025-08-27 14:43:26 +0300

    Added Some Buttons
    Buttons For moving back to the cube in case the camera gets buggy/ user gets lost.
    Mute Button for music
    Bigger Sky/environment

--- main.py
+++ main.py
@@ -22,13 +22,11 @@ shuffleOn = False
 
 # This is where we make the sky.
 sky_texture = load_texture('bg.png')
-sky_texture.wrap_mode = 'repeat'   # I made the texture repeat instead of clamp
-
 # We Created a big cube that surrounds everything
 sky = Entity(
     model='cube',
     texture=sky_texture,
-    scale=100,   # really big so you never reach the edge
+    scale=8000,   # really big so you never reach the edge
     double_sided=True
 )
 
@@ -38,14 +36,19 @@ sky.texture_scale = (1, 1)     # 1 means texture covers the whole face once
 # THis is where some UI and background music is defined.
 window.editor_ui.disable()
 background_music = Audio('background_music.mp3',
-                         loop=True, autoplay=True, volume=0.5)
+                         loop=True, autoplay=True,)
+background_music.volume = 0.5
+
 my_text = Text(text="CQUIRRELS CUBE!", scale=3, origin=(0.7, -5.5))
 my_text = Text(text="Space Bar+LMB: Y axis rotation",
                scale=1, origin=(1.55, -14))
 my_text = Text(text="Z+LMB: Z axis rotation", scale=1, origin=(2.34, -13))
 my_text = Text(text="Shift+any previous rotation binds: spin in another direction",
                scale=1, origin=(.576, -12))
+my_text = Text(text="M to Mute the Music",
+               scale=1, origin=(2.5, 1))
 button = Button(scale=(.2, .1), text='Toggle Shuffle', origin=(3.2, -2))
+
 button.text_entity.world_scale = 20
 
 # This is where the button is given an action when pressed.
@@ -231,6 +234,14 @@ def input(key):
     HeldZ = held_keys['z']
     heldShift = held_keys['left shift']
 
+    if key == 'm':
+        if background_music.playing:
+            background_music.pause()
+            print("Music Paused")
+        else:
+            background_music.resume()
+            print("Music Resumed")
+
     # THis code detects if just left mouse was pressed, and moves the cube accordingly.
     # This makes sure that the cube only moves if it isn't currently moving, and if the only input is left click.
     if key == 'left mouse down' and not heldSpace and not isRotating and not HeldZ:
@@ -288,6 +299,38 @@ def input(key):
                 sound_effect = Audio('coin', autoplay=True)
 
 
-EditorCamera()  # This turns the editor camera on so you can move around.
+# This turns the editor camera on so you can move around.
+camera_editor = EditorCamera()
+
+
+def back_to_cube():
+    camera_editor.position = (.5, 0, 0)
+    camera_editor.rotation = (0, 0, 0)
+
+# Moves Editor Cam Back to the Cube
+
+
+def face_top():
+    camera_editor.position = (0, 0, 0)
+    camera_editor.rotation = (45, 0, 0)
+# This Makes the Cam Face the topp
+
+
+def face_bottom():
+    camera_editor.position = (0, 0, 0)
+    camera_editor.rotation = (-45, 0, 0)
+# This Makes the Cam Face the bottom
+
+
+bc = Button(text='Back to Cube', scale=(.2, .1), position=(.2, .4))
+bc.on_click = back_to_cube
+
+ft = Button(text='Face Cube top', scale=(.2, .1), position=(.4, .4))
+ft.on_click = face_top
+
+fb = Button(text='Face Cube bottom', scale=(.2, .1), position=(.6, .4))
+fb.on_click = face_bottom
+
+
 app.run()  # This runs the code.
 # We hope you enjoyed learning how this code works, it was a super hard challenge, but was also really fun!
